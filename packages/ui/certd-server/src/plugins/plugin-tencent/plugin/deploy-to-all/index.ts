32c714d1b6e68c71a74a7452115040c87ac4bfdc
xinghejd <31512683+xinghejd@users.noreply.github.com>
2025-04-15 07:25:12 +0000

    fix: 修复腾讯云部署到任意资源插件，无法使用之前已上传的腾讯云证书问题

--- packages/ui/certd-server/src/plugins/plugin-tencent/plugin/deploy-to-all/index.ts
+++ packages/ui/certd-server/src/plugins/plugin-tencent/plugin/deploy-to-all/index.ts
@@ -128,11 +128,12 @@ export class DeployCertToTencentAll extends AbstractTaskPlugin {
     });
 
     let certId:string = null
-    if (typeof certId === 'object') {
-      //上传
-      certId = await this.uploadToTencent(access,this.tencentCertId as CertInfo);
+    if (typeof this.tencentCertId === 'string') {
+      certId = this.tencentCertId;
+    } else if (this.tencentCertId && typeof this.tencentCertId === 'object') {
+      certId = await this.uploadToTencent(access, this.tencentCertId);
     } else {
-      certId = this.tencentCertId  as string;
+      throw new Error('无效的证书输入类型');
     }
 
     const params = {
