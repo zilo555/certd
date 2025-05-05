11801d8e2e9ecaf93069e40b07a89381bc690a1f
xiaojunnuo <xiaojunnuo@qq.com>
2025-05-05 22:20:42 +0800

    chore:

--- packages/plugins/plugin-cert/src/plugin/cert-plugin/base-convert.ts
+++ packages/plugins/plugin-cert/src/plugin/cert-plugin/base-convert.ts
@@ -27,7 +27,8 @@ export abstract class CertApplyBaseConvertPlugin extends AbstractTaskPlugin {
       "1、支持多个域名打到一个证书上，例如： foo.com，*.foo.com，*.bar.com\n" +
       "2、子域名被通配符包含的不要填写，例如：www.foo.com已经被*.foo.com包含，不要填写www.foo.com\n" +
       "3、泛域名只能通配*号那一级（*.foo.com的证书不能用于xxx.yyy.foo.com、不能用于foo.com）\n" +
-      "4、输入一个，空格之后，再输入下一个",
+      "4、输入一个，空格之后，再输入下一个 \n" +
+      "5、如果你配置了子域托管解析，请先[设置托管子域名](#/certd/pipeline/subDomain)",
   })
   domains!: string[];
 
