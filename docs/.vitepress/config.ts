bedb1ff7f9ca9b7b32c62e5cac5b84813779efe8
xiaojunnuo <xiaojunnuo@qq.com>
2025-12-01 11:48:57 +0800

    docs: 流水线有效期

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -121,6 +121,7 @@ export default defineConfig({
                         {text: "ESXi", link: "/guide/use/ESXi/index.md"},
                         {text: "宝塔动态IP白名单", link: "/guide/use/baota/white_list.md"},
                         {text: "子域名托管", link: "/guide/use/cert/subdomain.md"},
+                        {text: "流水线有效期", link: "/guide/use/pipeline/valid.md"},
                     ]
                 },
                 {
---
+++ docs/guide/use/pipeline/valid.md
@@ -0,0 +1,17 @@
+# 流水线有效期功能
+
+可以为流水线设置有效期，超过有效期后，流水线将停止运行
+
+## 1. 打开有效期开关
+
+![setting.png](images/setting.png)
+
+## 2. 设置有效期
+
+![valid.png](images/edit.png)
+
+![valid.png](images/edit2.png)
+
+## 3. 设置完成
+该流水线将在有效期结束后停止运行
+![valid.png](images/show.png)
