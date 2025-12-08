45fbce0c2af5fb3ead6d3dd12a42f8cc1714262f
xiaojunnuo <xiaojunnuo@qq.com>
2025-12-09 00:45:10 +0800

    perf: 新增数据库迁移doc说明文档，优化datetime字段平滑迁移

---
+++ packages/ui/certd-server/db/migration-mysql/v10035__transforme_fix.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `sys_settings` MODIFY COLUMN `setting` longtext  NULL;
+ALTER TABLE `user_settings` MODIFY COLUMN `setting` longtext  NULL;
---
+++ packages/ui/certd-server/db/migration-pg/v10035__transforme_fix.sql
@@ -0,0 +1,2 @@
+alter table sys_settings alter column setting type text using setting::text;
+alter table user_settings alter column setting type text using setting::text;
