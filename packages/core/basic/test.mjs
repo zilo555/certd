b91548eef4c24faa822d3a40f1f6a77b41d274e4
xiaojunnuo <xiaojunnuo@qq.com>
2026-02-13 19:02:53 +0800

    perf: 421 支持3次重试

--- packages/core/basic/test.mjs
+++ packages/core/basic/test.mjs
@@ -13,6 +13,19 @@
 
 // await testLocker();
 
-import { domainUtils } from "./dist/utils/util.domain.js";
+// import { domainUtils } from "./dist/utils/util.domain.js";
 
-console.log(domainUtils.isIpv6("::0:0:0:FFFF:129.144.52.38"));
+// console.log(domainUtils.isIpv6("::0:0:0:FFFF:129.144.52.38"));
+
+// import { http } from "./dist/utils/util.request.js";
+
+// http
+//   .request({
+//     url: "https://www.baidu.com/234234/3333",
+//     retry: {
+//       status: [404],
+//     },
+//   })
+//   .then(res => {
+//     console.log(res.data);
+//   });
